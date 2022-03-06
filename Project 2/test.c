54097aa7725215079af05bff124715170cfac63b
Brandon <67395281+brandonmaciel@users.noreply.github.com>
2022-03-06 01:15:16 -0600

    Add files via upload

---
+++ Project 2/test.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+typedef struct Customer {
+    int ID;                         // (key/value) Unique 4 digit number (0-9999). ignore trailling zeros
+    char name[100];
+    float store_balance;            // store credit accrued
+    float order_total;              // total cost of order
+}customer_t;
+
+typedef struct stack{
+    char 
+
+}stack_t;
+
+void parseCustomers(char *CustInfoString, customer_t* customer){
+    
+}
+
+int main(void){
+
+    return 0;
+}
