7bd23848a1b129618c707b5b9a6f857a77e5f280
Brandon <67395281+brandonmaciel@users.noreply.github.com>
2022-03-06 01:11:03 -0600

    Delete Labs directory

--- Labs/Lab 12/Lab12.h
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int scan_crew_data(crew_data_t *crew_data){
-    
-    return 0;
-}
--- Labs/Lab 9/tempCodeRunnerFile.c
+++
@@ -1,3 +0,0 @@
-    char y[80] = "giraffe"; 
-    strcat(x, y);
-    printf("%s %s\n", x, y);
